faf75f67ce498a8c97591f5a68cadac35a2a6e5e
Nico Mamone <nicolasmamone@gmail.com>
2024-03-18 20:05:05 -0300

    creando componente empleado

---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,13 @@
+ //Export -> para que sea accesible desde otro archivo
+
+import { Component } from "@angular/core";
+
+ @Component({
+    selector: 'app-empleados',
+    templateUrl: './empleados.component.html',
+    styleUrls: ['./empleados.component.css']
+
+ })
+ export class EmpleadosComponent{
+     
+ }
