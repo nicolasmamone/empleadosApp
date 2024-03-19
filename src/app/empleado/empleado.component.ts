11576b4053c1aac05a8b6c6b77073f5b86c469da
Nico Mamone <nicolasmamone@gmail.com>
2024-03-19 20:16:42 -0300

    viendo interpolacion

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrl: './empleado.component.css'
+  styleUrl: './empleado.component.css',
+  //styles: [' p{background-color:yellow;}   ']
 })
-export class EmpleadoComponent {
+export class EmpleadoComponent implements OnInit {
+  
+  private nombre = 'Pepe';
+  private apellido = 'Argento';
+  private edad = 45;
+  //private empresa = 'Telefonica';
+
+  //Metodos get
+
+  getNombre(){
+    return this.nombre;
+  }
+  getApellido(){
+    return this.apellido;
+  }
+
+  getEdad(){
+    return this.edad;
+  }
+  
+  /*
+  getEmpresa(){
+    return this.empresa;
+  } */
+  
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
